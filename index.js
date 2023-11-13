a93dc86b17235f9acb7bfcab5ce0f5062101f897
saeidha9498 <saeidha948@gmail.com>
2023-11-14 03:13:48 +0330

    fix

---
+++ index.js
@@ -0,0 +1,26 @@
+// importing modules here
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// importing different files
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+const app = express();
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`App is running on port no 5000`);
+});
